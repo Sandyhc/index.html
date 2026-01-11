97f8e47dd51719b5f9b7fff850992e5ea45d03d7
Sandyhc <103366988+Sandyhc@users.noreply.github.com>
2026-01-11 10:48:52 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -4,30 +4,20 @@ export default function handler(req, res) {
   if (!type || !value) {
     return res.status(400).json({
       success: false,
-      error: "type aur value required hai"
+      error: "Missing type or value"
     });
   }
 
-  if (type === "number") {
-    return res.status(200).json({
-      success: true,
-      type,
-      value,
-      message: "Mobile number API working"
-    });
-  }
-
-  if (type === "aadhaar") {
-    return res.status(200).json({
-      success: true,
-      type,
-      value,
-      message: "Aadhaar API working (demo)"
-    });
-  }
-
-  return res.status(400).json({
-    success: false,
-    error: "Invalid type"
+  res.status(200).json({
+    success: true,
+    type,
+    owner: "Sandeep",
+    result: [
+      {
+        value: value,
+        name: "Demo User",
+        address: "India"
+      }
+    ]
   });
 }
